b556dbbdfa60c80e4ce8ac9f2610e7fa1e095c86
Elvys <elvysd@gmail.com>
2021-07-27 13:50:19 -0700

    UI updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import _ from 'lodash';
 //import printMe from './print.js';
 import './style.css';
-import {MDCRipple} from '@material/ripple';
+import {
+    MDCRipple
+} from '@material/ripple';
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
 const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
 
-console.log("sssssssssd");
+import {
+    MDCList
+} from "@material/list";
+const list = MDCList.attachTo(document.querySelector('.mdc-list'));
+list.wrapFocus = true;
+import {
+    MDCDrawer
+} from "@material/drawer";
+
+const listEl = document.querySelector('.mdc-drawer .mdc-list');
+const mainContentEl = document.querySelector('.main-content');
+
+listEl.addEventListener('click', (event) => {
+    mainContentEl.querySelector('input, button').focus();
+});
+
+
+
+import {MDCDataTable} from '@material/data-table';
+const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
+// Main code
 
